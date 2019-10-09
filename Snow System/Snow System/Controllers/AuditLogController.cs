d4a8e4f6ee507d4bc27fc6c6c587113489191cd9
Justin <14369852@tuks.co.za>
2019-10-09 17:49:19 +0200

    Product Order Started

--- Snow System/Snow System/Controllers/AuditLogController.cs
+++ Snow System/Snow System/Controllers/AuditLogController.cs
@@ -13,17 +13,6 @@ namespace Snow_System.Controllers
         SpartanFireDBEntities1 db = new SpartanFireDBEntities1();
         public ActionResult Index(string searchBy, string search)
         {
-
-            //n.AuditLogID += 1;
-            //n.DateAccessed = DateTime.Now;
-            //n.TableAccessed = "Client";
-            //n.InformationChanged = "Viewed clients";
-            //n.NewInformationEntered = "Viewed clients";
-            //n.AuditLogTypeID = 1;
-            //n.UserID = c.UserID;
-            //db.AuditLogs.Add(n);
-            //db.SaveChanges();
-            //var account = db.Users.Where(a => a.UserID == id).FirstOrDefault();
             if (searchBy == "UserID")
             {
                 return View(db.AuditLogs.Where(x => x.User.UserEmail== search || search == null).ToList());
