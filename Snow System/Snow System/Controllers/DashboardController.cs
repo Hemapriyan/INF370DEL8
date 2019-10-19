9095b3fb2007afe7c47adbbeddd53af3fc831a54
Justin Michel <u14369852@tuks.co.za>
2019-10-19 13:44:19 +0200

    Tasks for days done

--- Snow System/Snow System/Controllers/DashboardController.cs
+++ Snow System/Snow System/Controllers/DashboardController.cs
@@ -16,11 +16,11 @@ namespace Snow_System.Controllers
         // GET: Dashboards
         public ActionResult Index()
         {
-            ViewBag.Deliveries = db.Deliveries.ToList(); //test
-            ViewBag.Services = db.ServiceRequests.Where(srv=>srv.ServiceBookedDate == DateTime.Today.Date).ToList();
+            ViewBag.Deliveries = db.Deliveries.Where(del=>del.DateOfDelivery == DateTime.Today.Date).ToList(); //test
+            ViewBag.Services = db.ServiceRequests.Where(srv=>srv.ServiceBookedDate== DateTime.Today.Date).ToList();
             ViewBag.ProductOrders = db.ProductOrders.Where(o => o.ProductOrderStatusID == 2).ToList();
             ViewBag.AllProductOrders = db.ProductOrders.Where(o => o.ProductOrderStatusID > 1 && o.ProductOrderStatusID < 7).ToList();
-            ViewBag.SupplierOrders = db.SupplierOrders.Where(o => o.SupplierStatusID == 2 || o.SupplierStatusID == 3).ToList();
+            ViewBag.SupplierOrders = db.SupplierOrders.Where(o => o.SupplierStatusID == 2 || o.SupplierStatusID == 6).ToList();
             ViewBag.Locations = db.Locations.ToList();
             ViewBag.Ratings = db.ClientFeedbacks.ToList();
             ViewBag.UnassignedDeliveries = db.Deliveries.Where(del => del.DeliveryStatusID == 1).ToList(); // check back
